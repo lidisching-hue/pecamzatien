90350572d3af77c1f3d8dec5c9bd55cb12ce4588
Lidis Chinguel <lidisching@gmail.com>
2025-12-23 00:07:00 -0500

    fix vercel SPA routin

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
 import { CartProvider } from './context/CartContext'
 import App from './App'
@@ -10,9 +9,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HelmetProvider>
       <CartProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+        <App />
       </CartProvider>
     </HelmetProvider>
   </StrictMode>
